260783625c8ee68984b576de50a500b4ce0e310f
Santosh Kumar <santosh.283143@gmail.com>
2023-06-11 18:09:35 +0530

    updated

--- model/todoModel.js
+++ model/todoModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const assert = require('assert')
 
-const connect = mongoose.createConnection(process.env.TODO_MONGO_URL, (err) => {
+const connect = mongoose.createConnection(process.env.TODO_MONGO_URL || "mongodb+srv://santosh283143:santosh1437@cluster0.nzq3jw9.mongodb.net/todo-list?retryWrites=true&w=majority&authSource=admin&ssl=true", (err) => {
     if (err) assert.deepStrictEqual(err, null)
     // console.log('mongoDB connected successfully');
 });
